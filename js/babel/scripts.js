1fef04880d37d7ad5e7eb75f9f48a3142fb9d3d6
Ricky <rickyjoet@gmail.com>
2023-04-30 20:57:32 -0700

    coded js mobile nav function and footer date

--- js/babel/scripts.js
+++ js/babel/scripts.js
@@ -6,5 +6,16 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 
 (0, _jquery["default"])(document).ready(function () {
   console.log("jquery ready");
+  (0, _jquery["default"])('.mobile-nav .lines').on('click', function (e) {
+    console.log('mobile nav clicked');
+    (0, _jquery["default"])('.navigation ul').addClass('show-nav');
+  });
+  (0, _jquery["default"])('.navigation ul li.mobile .lines').on('click', function (e) {
+    console.log('clicked close icon');
+    (0, _jquery["default"])('.navigation ul').removeClass('show-nav');
+  });
+  var date = new Date();
+  date = date.getFullYear();
+  (0, _jquery["default"])('.year').text(date);
 });
 //# sourceMappingURL=scripts.js.map
--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,22 @@ import $ from 'jquery';
 
 $(document).ready(function() {
   console.log(`jquery ready`);
+
+
+$('.mobile-nav .lines').on('click', function(e) {
+  console.log('mobile nav clicked');
+  $('.navigation ul').addClass('show-nav');
+
+});
+
+$('.navigation ul li.mobile .lines').on('click', function(e) {
+  console.log('clicked close icon');
+  $('.navigation ul').removeClass('show-nav');
+
+});
+
+var date = new Date();
+date = date.getFullYear();
+$('.year').text(date);
+
 });
